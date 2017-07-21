615fb26aaf5a09ae326cd1ff3f75dcfeb2b748a3
木宛城主 <1194679215@qq.com>
2017-07-22 00:09:54 +0800

    ContentCategoryMapper.xml逆向工程返回自增长主键 select last_insert＿id

--- itao-manager-web/src/main/java/com/itao/controller/ContentController.java
+++ itao-manager-web/src/main/java/com/itao/controller/ContentController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.itao.common.utils.E3Result;
+import com.itao.content.service.ContentCategoryService;
 import com.itao.content.service.ContentService;
 import com.itao.pojo.TbContent;
 
