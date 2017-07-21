4b19fb39fc5b05f3d712c2b5b24062fe40cec07d
木宛城主 <1194679215@qq.com>
2017-07-22 00:30:07 +0800

    redis对首页轮播图以hset形式添加缓存

--- itao-content/itao-content-service/src/main/java/com/itao/content/service/impl/ContentServiceImpl.java
+++ itao-content/itao-content-service/src/main/java/com/itao/content/service/impl/ContentServiceImpl.java
@@ -3,10 +3,14 @@ package com.itao.content.service.impl;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.itao.common.jedis.JedisClient;
 import com.itao.common.utils.E3Result;
+import com.itao.common.utils.JsonUtils;
 import com.itao.content.service.ContentService;
 import com.itao.mapper.TbContentMapper;
 import com.itao.pojo.TbContent;
@@ -16,26 +20,51 @@ import com.itao.pojo.TbContentExample.Criteria;
 @Service
 public class ContentServiceImpl implements ContentService {
 
+	@Value("${CONTENT_LIST}")
+	private String CONTENT_LIST;
+	
 	@Autowired
 	private TbContentMapper contentMapper;
+	@Autowired
+	private JedisClient jedisClient;
 	@Override
 	public E3Result addContent(TbContent content) {
 		
 		content.setCreated(new Date());
 		content.setUpdated(new Date());
 		contentMapper.insert(content);
-		
+		//缓存同步
+		jedisClient.hdel(CONTENT_LIST, content.getCategoryId().toString());
 		return E3Result.ok();
 	}
 
 	@Override
 	public List<TbContent> getCotentListByCid(long cid) {
 		
+		try {
+			//如果缓存中有则直接返回
+			String json=jedisClient.hget(CONTENT_LIST, cid+"");
+			if (StringUtils.isNoneBlank(json)) {
+				List<TbContent> list = JsonUtils.jsonToList(json, TbContent.class);
+				return list;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		//如果没有则查询数据库
 		TbContentExample example=new TbContentExample();
 		Criteria criteria = example.createCriteria();
 		criteria.andCategoryIdEqualTo(cid);
 		
 		List<TbContent> list = contentMapper.selectByExampleWithBLOBs(example);
+		//把结果添加到缓存中去
+		try {
+			jedisClient.hset(CONTENT_LIST, cid+"", JsonUtils.objectToJson(list));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 		return list;
 		
 	}
