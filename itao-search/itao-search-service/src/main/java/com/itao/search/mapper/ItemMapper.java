82c7a79e6099a10fd4986f8fc93123622366ae7b
木宛城主 <1194679215@qq.com>
2017-07-23 17:39:20 +0800

    使用solrJ实现搜索

---
+++ itao-search/itao-search-service/src/main/java/com/itao/search/mapper/ItemMapper.java
@@ -0,0 +1,12 @@
+package com.itao.search.mapper;
+
+import java.util.List;
+
+import com.itao.common.pojo.SearchItem;
+import com.itao.common.pojo.SearchResult;
+
+
+public interface ItemMapper {
+
+	List<SearchItem> getItemList();
+}
